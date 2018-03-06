be0befbf288db303758bfb47bc7ae9e9a25a32c5
GYCHEN <gy.chen@gms.nutc.edu.tw>
2018-03-06 09:44:38 +0000

    WIP: don't know this will working or not

---
+++ README.md
@@ -0,0 +1,9 @@
+### uppy-plugin-socketio
+An uppy uploader plugin to upload files through SocketIO.
+
+#### TODOs
+
+- [ ] uppy plugin socketio
+  - [ ] Provide parameter that can specific SocketIO channel.
+  - [ ] Provide parameter socketio that can combine uploader to exists SocketIO object.
+- [ ] Need webpack or something like that to translate code. 
---
+++ src/plugin/SocketIOUploader.js
@@ -0,0 +1,70 @@
+import io from 'socket.io-client';
+import Plugin from 'uppy/src/core/Plugin';
+
+
+class SocketIOUploader extends Plugin {
+    constructor(uppy, opts) {
+        super(uppy, opts);
+        this.type = 'uploader';
+        this.id = 'SocketIOUploader';
+        this.title = 'SocketIOUploader';
+
+        const defaultOptions = {
+            channel: '/file',
+            // provide socketio object or socketio server url
+            socketio: null
+        };
+
+        this.opts = Object.assign({}, defaultOptions, opts);
+
+        this.handleUpload = this.handleUpload.bind(this);
+        this.uploadFile = this.uploadFile.bind(this);
+        this._initialize_socketio = this._initialize_socketio.bind();
+
+        this._initialize_socketio(this.opts.socketio);
+    }
+
+    _initialize_socketio(socketio) {
+        if (instanceof(socketio, object)) {
+            this._io = socketio;
+        } else if (instanceof(socketio, string)) {
+            this._io = io(socketio);
+        } else {
+            throw new Error("Please provide socketio opt.");
+        }
+    }
+
+    /**
+     * upload file
+     *
+     * @param fileID
+     * @return promise
+     */
+    uploadFile(fileID) {
+        const file = this.uppy.get(fileID);
+        return new Promise((resolve, reject) => {
+            this.uppy.emit('upload-started', file.id)
+            this._io.emit(this.opts.channel, file.data, data => {
+                // server need to send ack data
+                this.uppy.emit('upload-success', file.id);
+            });
+        });
+    }
+
+    handleUpload(fileIDs) {
+        const promises = fileIDs.map(fileId => {
+            return this.uploadFile(fileId);
+        });
+        return settle(promises);
+    }
+
+    install() {
+        this.uppy.addUploader(this.handleUpload);
+    }
+
+    uninstall() {
+        this.uppy.removeUploader(this.handleUpload);
+    }
+}
+
+export default SocketIOUploader;
