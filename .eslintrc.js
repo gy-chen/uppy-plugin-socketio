60aeee060ee2d9937b290bb889601cd3e7535942
GYCHEN <gy.chen@gms.nutc.edu.tw>
2018-03-23 03:36:23 +0100

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "plugins": ["prettier"],
+    "rules": {
+        "prettier/prettier": "error"
+    },
+    "parserOptions": {
+        "ecmaVersion": 8,
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true
+        }
+    },
+};
