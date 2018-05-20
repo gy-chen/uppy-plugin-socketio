7bbc56512575f28edf0cdb940de364045d02aad2
GYCHEN <gy.chen@gms.nutc.edu.tw>
2018-05-20 16:44:50 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,20 @@
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
+    devtool: 'source-map',
     entry: './src/plugin/SocketIOUploader.js',
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new UglifyJSPlugin({
+            sourceMap: true
+        })
+    ],
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
-        library: 'SocketIOUpload'
+        library: 'SocketIOUpload',
+        libraryTarget: 'umd'
     }
 }
