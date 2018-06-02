0775c15897c1343b2df5d03db00b02c3c19c9989
GYCHEN <gy.chen@gms.nutc.edu.tw>
2018-06-02 19:56:50 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,34 @@
-const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const path = require("path");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = {
-    devtool: 'source-map',
-    entry: './src/plugin/SocketIOUploader.js',
-    plugins: [
-        new CleanWebpackPlugin(['dist']),
-        new UglifyJSPlugin({
-            sourceMap: true
-        })
-    ],
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        library: 'SocketIOUpload',
-        libraryTarget: 'umd'
-    }
-}
+  devtool: "source-map",
+  entry: "./src/plugin/SocketIOUploader.js",
+  plugins: [
+    new CleanWebpackPlugin(["dist"]),
+    new UglifyJSPlugin({
+      sourceMap: true
+    })
+  ],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    library: "SocketIOUpload",
+    libraryTarget: "umd"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  }
+};
