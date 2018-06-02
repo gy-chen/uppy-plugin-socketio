acb1bbcd4dc154b07bdbb1a05d2ef979463b9af1
GYCHEN <gy.chen@gms.nutc.edu.tw>
2018-06-02 20:25:00 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -21,13 +21,8 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"]
-          }
-        }
+        exclude: /node_modules/,
+        loader: "babel-loader"
       }
     ]
   }
